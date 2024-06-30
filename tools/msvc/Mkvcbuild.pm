8e09a48f93957d7c34146d2825559158329fc9a4
Miron Berlin <berlinm3@users.noreply.code.roche.com>
2024-06-29 23:08:12 -0700

    MS Windows port for branch PG16 catch up

--- tools/msvc/Mkvcbuild.pm
+++ tools/msvc/Mkvcbuild.pm
@@ -789,6 +789,7 @@ sub GenerateRegressionSqlFile
 	 'scan.sql'
    , 'graphid.sql'
    , 'agtype.sql'
+   , 'agtype_hash_cmp.sql'
    , 'catalog.sql'
    , 'cypher.sql'
    , 'expr.sql'
@@ -803,6 +804,7 @@ sub GenerateRegressionSqlFile
    , 'cypher_union.sql'
    , 'cypher_call.sql'
    , 'cypher_merge.sql'
+   , 'cypher_subquery.sql'
    , 'age_global_graph.sql'
    , 'age_load.sql'
    , 'index.sql'
@@ -810,6 +812,8 @@ sub GenerateRegressionSqlFile
    , 'graph_generation.sql'
    , 'name_validation.sql'
    , 'jsonb_operators.sql'
+   , 'list_comprehension.sql'
+   , 'map_projection.sql'
    , 'drop.sql');
 
 	my $copy_index = 0;
